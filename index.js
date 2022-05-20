da33271c6e21300a18b1c82407580a8e02246f82
Jake <jakespeitel19@gmail.com>
2022-05-20 12:57:03 -0400

    commit tree 140a23abfed9dafc16eed749ff788cf4aa012096

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+const hqStreet = 42;
+
+
+function distanceFromHqInBlocks(userStreet){
+
+    if(userStreet < 42){
+        return (hqStreet - userStreet);
+    } else {
+        return (userStreet - hqStreet);
+    }
+}
+
+function distanceFromHqInFeet(userStreet){
+    return (distanceFromHqInBlocks(userStreet) * 264);
+}
+
+function distanceTravelledInFeet(start, des){
+    if(start - des > 0) {
+        return ((start - des) * 264);
+    } else {
+        return ((des - start) * 264);
+    }
+}
+function calculatesFarePrice(start, des){
+    let disTra = distanceTravelledInFeet(start, des);
+    if(disTra <= 400){
+        return 0;
+    }
+    if(disTra > 400 && disTra <= 2000){
+        return ((disTra - 400) * .02);
+    }
+    if(disTra > 2000 && disTra <= 2500){
+        return 25;
+    }
+    if(disTra > 2500){
+        return "cannot travel that far";
+    }
+}
